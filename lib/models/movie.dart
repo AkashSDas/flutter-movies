d6731d03cac98d497a4c95b7ee62e0db9cd8dff9
Akash Das <aakashdas368@gmail.com>
2020-11-08 00:16:39 +0530

    change Movie class year dtype and fromJson method

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -1,11 +1,8 @@
 class Movie {
   final String title;
-  final int year;
+  final String year;
   final String posterImg;
   final String type; // eg: "movie"
-
-  static const defaultPostImg =
-      'https://images.unsplash.com/photo-1603568301602-3924e87dc8d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80';
 
   Movie({
     this.title,
@@ -17,8 +14,8 @@ class Movie {
   // semi-colon is to assign final fields before the body
   // is executed
   Movie.fromJson(Map<String, dynamic> json)
-      : title = json['Title'] ? json['Title'] : 'No title',
-        year = json['Year'] ? json['Year'].toInt() : -1,
-        posterImg = json['Poster'] ? json['Poster'] : Movie.defaultPostImg,
+      : title = json['Title'],
+        year = json['Year'],
+        posterImg = json['Poster'],
         type = json['Type'];
 }

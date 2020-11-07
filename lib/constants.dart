097b44d843792efdcced305e5320ac33462a43e8
Akash Das <aakashdas368@gmail.com>
2020-11-07 13:56:29 +0530

    add fontFamilies in constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,3 +6,10 @@ const secondaryBg = Color(0xFF23272a);
 const iconColor = Color(0xFF99aab5);
 const textColor = Color(0xFFffffff);
 const btnPrimaryColor = Color(0xFF7289da);
+const green = Color(0xFF73DA22);
+
+// fonts
+const logoFont = 'PorterSansBlock';
+const headlineFont = 'Montserrat';
+const btnFont = 'Montserrat';
+const bodyFont = 'Nunito';

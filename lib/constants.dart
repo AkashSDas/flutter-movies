d36ced85a7fc088f40a64b98e9301a6f35a351d6
Akash Das <aakashdas368@gmail.com>
2020-11-07 13:32:10 +0530

    add colors for app in constant.dart

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// colors
+const primaryBg = Color(0xFF2c2f33);
+const secondaryBg = Color(0xFF23272a);
+const iconColor = Color(0xFF99aab5);
+const textColor = Color(0xFFffffff);
+const btnPrimaryColor = Color(0xFF7289da);

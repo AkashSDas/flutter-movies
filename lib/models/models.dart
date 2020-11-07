2ed76613ac52ed4b0d5062d1062027b82371ef96
Akash Das <aakashdas368@gmail.com>
2020-11-07 19:01:22 +0530

    add MovieResponse in movie_response.dart file

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,1 +1,2 @@
-export 'movie.dart';
+export 'package:flutter_movies/models/movie.dart';
+export 'package:flutter_movies/models/movie_response.dart';
---
+++ lib/models/movie_response.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_movies/models/movie.dart';
+
+class MovieResponse {
+  List<Movie> movies;
+  String error;
+  String notFoundMsg;
+
+  MovieResponse({this.movies, this.error, this.notFoundMsg});
+
+  MovieResponse.fromJson(Map<String, dynamic> json) {
+    if (json['Response'] == 'False') {
+      // print(json); // { Response: 'False', Error: 'Movie not found!' }
+      this.notFoundMsg = 'Movie not found';
+      this.movies = [];
+      this.error = '';
+    } else {
+      // json['Response'] == 'True'
+      // print(json);
+      this.movies =
+          (json['Search'] as List).map((m) => Movie.fromJson(m)).toList();
+      this.error = '';
+      this.notFoundMsg = '';
+    }
+  }
+
+  MovieResponse.withError(String errorValue) {
+    this.movies = [];
+    this.error = errorValue;
+    this.notFoundMsg = '';
+  }
+}

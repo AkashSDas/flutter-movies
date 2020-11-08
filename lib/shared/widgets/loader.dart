f27c5879f1a02bddaa0ec2d795ed699551f37754
Akash Das <aakashdas368@gmail.com>
2020-11-08 12:44:51 +0530

    change Loader color

--- lib/shared/widgets/loader.dart
+++ lib/shared/widgets/loader.dart
@@ -4,16 +4,21 @@ import 'package:flutter_movies/constants.dart';
 class Loader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(space),
-      width: space * 10,
-      height: space * 10,
-      child: Align(
-        alignment: Alignment.center,
-        child: Container(
-          width: space * 10,
-          height: space * 10,
-          child: CircularProgressIndicator(strokeWidth: 8),
+    return Center(
+      child: Container(
+        margin: EdgeInsets.all(space),
+        width: space * 10,
+        height: space * 10,
+        child: Align(
+          alignment: Alignment.center,
+          child: Container(
+            width: space * 10,
+            height: space * 10,
+            child: CircularProgressIndicator(
+              backgroundColor: btnPrimaryColor,
+              strokeWidth: 8,
+            ),
+          ),
         ),
       ),
     );

2cacd0bfbc1af078daf6faa151b222392dc6ef7b
Akash Das <aakashdas368@gmail.com>
2020-11-08 18:24:02 +0530

    add default poster img link

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -16,6 +16,8 @@ class Movie {
   Movie.fromJson(Map<String, dynamic> json)
       : title = json['Title'],
         year = json['Year'],
-        posterImg = json['Poster'],
+        posterImg = json['Poster'] != 'N/A'
+            ? json['Poster']
+            : 'https://images.unsplash.com/photo-1481278403982-f2d9f387cdcc?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
         type = json['Type'];
 }

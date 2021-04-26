d47a108de9aad9e832ea9fb6b347c9b604f58176
AkashSDas <aakashdas368@gmail.com>
2021-04-26 13:53:15 +0530

    add style.dart

---
+++ lib/style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// spacing constant
+const double space = 10;
+
+/// colors
+const Color primaryColor = Color(0xFF1B1B1B);
+const Color accentColor = Color(0xFFFF8A00);
+
+/// fonts
+const fontHead = 'OpenSans';
+const fontBody = 'SourceCodePro';

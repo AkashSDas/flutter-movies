2be6eb2d8af5eb7210fb8bb2f6eaccb939561379
AkashSDas <aakashdas368@gmail.com>
2021-04-26 15:57:11 +0530

    add HomeScreen.dart and export it

---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_movies/style.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Style.accentColor,
+    );
+  }
+}

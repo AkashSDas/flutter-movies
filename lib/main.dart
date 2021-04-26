c75ec9a932bc030f03651c38767b5703f662d1c5
AkashSDas <aakashdas368@gmail.com>
2021-04-26 15:56:35 +0530

    implement splash screen in main.dart and wrap the constants in style.dart in side Style class

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,34 @@
+import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_movies/screens/HomeScreen.dart';
+import 'package:flutter_movies/style.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarBrightness: Brightness.light,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.light,
+    ));
+
+    return MaterialApp(
+      title: 'Flutter Movies',
+      debugShowCheckedModeBanner: false,
+      theme: Style.themeData,
+      routes: {
+        '/': (context) => SplashScreen.navigate(
+              name: 'assets/animations/flare-splash-screen.flr',
+              next: (context) => HomeScreen(),
+              until: () => Future.delayed(Duration(seconds: 3)),
+              startAnimation: 'go',
+              backgroundColor: Theme.of(context).primaryColor,
+            ),
+      },
+    );
   }
 }
--- lib/style.dart
+++ lib/style.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
-/// spacing constant
-const double space = 10;
+class Style {
+  /// spacing constant
+  static const double space = 10;
 
-/// colors
-const Color primaryColor = Color(0xFF1B1B1B);
-const Color accentColor = Color(0xFFFF8A00);
+  /// colors
+  static const Color primaryColor = Color(0xFF1B1B1B);
+  static const Color accentColor = Color(0xFFFF8A00);
 
-/// fonts
-const fontHead = 'OpenSans';
-const fontBody = 'SourceCodePro';
+  /// fonts
+  static const fontHead = 'OpenSans';
+  static const fontBody = 'SourceCodePro';
+
+  /// theme data
+  static final ThemeData themeData = ThemeData(
+    primaryColor: primaryColor,
+  );
+}

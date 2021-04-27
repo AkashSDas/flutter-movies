fa31862e8997b407047e9d022078adeebdc7ac25
AkashSDas <aakashdas368@gmail.com>
2021-04-27 11:59:38 +0530

    refactor coditional checking in movie.dart for Movie class

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -9,10 +9,10 @@ class Movie {
   // semi-colon is to assign final fields before the body
   // is executed
   Movie.fromJson(Map<String, dynamic> data)
-      : this.title = data['Title'] ? data['Title'] : 'No title',
-        this.year = data['Year'] ? data['Year'] : 'No year',
+      : this.title = data['Title'] != null ? data['Title'] : 'No title',
+        this.year = data['Year'] != null ? data['Year'] : 'No year',
         this.posterImg = data['Poster'] != 'N/A'
             ? data['Poster']
             : 'https://images.unsplash.com/photo-1481278403982-f2d9f387cdcc?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-        this.type = data['Type'] ? data['Type'] : 'No type';
+        this.type = data['Type'] != null ? data['Type'] : 'No type';
 }
